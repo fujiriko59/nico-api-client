b93b704e41b14ba40678575cab9e637181fba4bd
fujiriko59 <Nswnsw-1>
2013-04-29 00:06:27 +0900

    Add HttpClientSettings

---
+++ src/main/java/jp/niconico/api/http/HttpClientSetting.java
@@ -0,0 +1,32 @@
+package jp.niconico.api.http;
+
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.PoolingClientConnectionManager;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+
+import java.util.concurrent.TimeUnit;
+
+public class HttpClientSetting {
+    public DefaultHttpClient createHttpClient() {
+        SchemeRegistry schemeRegistry = new SchemeRegistry();
+        schemeRegistry.register(new Scheme("http", 80, PlainSocketFactory.getSocketFactory()));
+        schemeRegistry.register(new Scheme("https", 443, PlainSocketFactory.getSocketFactory()));
+
+        ClientConnectionManager clientConnectionManager = new PoolingClientConnectionManager(
+                schemeRegistry, 5, TimeUnit.MINUTES);
+
+        DefaultHttpClient httpClient = new DefaultHttpClient(clientConnectionManager);
+        HttpParams httpParams = httpClient.getParams();
+
+        HttpConnectionParams.setConnectionTimeout(httpParams, 10 * 1000);
+        HttpConnectionParams.setSoTimeout(httpParams, 10 * 1000);
+
+
+        return httpClient;
+    }
+}
